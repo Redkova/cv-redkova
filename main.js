41c25386f779cb5bb0b863587763cc746f73fbcf
Sandra Redkova <sandra_09_94@mail.ru>
2026-02-02 00:13:14 +0100

    feat: add main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector('.download-btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  const link = document.createElement('a');
+  link.href = 'files/Alexandra_Redkova_CV.pdf';
+  link.download = 'Alexandra_Redkova_CV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
